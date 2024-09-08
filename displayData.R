eab58d5e90f39ece1afc90ecd39072276dfd477e
Nicolas GONZALEZ <nicolas@thedeepcore.com>
2024-09-08 13:50:09 +0200

    Use mtext to render text details

--- displayData.R
+++ displayData.R
@@ -31,6 +31,7 @@ for (file in fnames) {
   nc_attributes <- ncatt_get(nc, variable)
   analysis      <- ncatt_get(nc, 0, "FORECAST")
   history       <- ncatt_get(nc, 0, "history")
+  time_long     <- ncatt_get(nc, "time", "long_name")
 
   r <- try(terra::rast(ndvi.array, ))
   r <- flip(r, direction="horizontal")
@@ -40,9 +41,17 @@ for (file in fnames) {
 
   for (i in 1:length(time)) {
     terra::plot(r[[i:i]],
-      main=nc_attributes$species,
       maxcell=1000000,
     )
+
+    # [1] "Carbon Monoxide µg/m3"
+    mtext(paste(nc_attributes$species, nc_attributes$units),
+      side = 3, line=3.2, cex=0.9)
+
+    # [1] "Model ENSEMBLE FORECAST 2024-09-01"
+    d <- strsplit(time_long$value, split=" ")[[1:1]][4]
+    mtext(paste(history$value, as.Date(d, "%Y%m%d") + i),
+      side = 3, line=2.7, cex=0.7)
   }
 
   dev.off()
